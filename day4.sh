4a893542a560702f523410ce18898228e354179f
Appavhargar <101106292+Appavhargar@users.noreply.github.com>
2022-03-07 19:04:17 +0530

    Add files via upload

---
+++ day4.sh
@@ -0,0 +1,11 @@
+for i in *
+do
+        n=`echo $i | awk -F. '{print $1}'`
+        n1=`echo $i | awk -F. '{print $2}'`
+        if [ -d $n ]
+        then
+                rm -rf $n
+        fi
+        mkdir -p $n/$n1
+        cp -r $i $n/$n1
+done
